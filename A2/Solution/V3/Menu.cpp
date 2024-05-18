bcf9e7bd2e85245d91232272c7492a7924ee6155
abdelrhman1304 <148173162+abdelrhman1304@users.noreply.github.com>
2024-05-18 15:39:50 +0300

    Update Menu.cpp
    Add reading from file option

--- A2/Solution/V3/Menu.cpp
+++ A2/Solution/V3/Menu.cpp
@@ -1,8 +1,6 @@
-#include "AVL.cpp"
-#include "BST.cpp"
-#include "Item.cpp"
-#include "MaxHeap.cpp"
-#include "MinHeap.cpp"
+#include "readitem.cpp"
+
+
 
 void menu() {
   BST bst;
@@ -11,49 +9,79 @@ void menu() {
   AVL avl;
 
   while (true) {
-    cout << "Choose a tree structure:\n";
-    cout << "1. Binary Search Tree\n";
-    cout << "2. Min-Heap\n";
-    cout << "3. Max-Heap\n";
-    cout << "4. AVL Tree\n";
-    cout << "5. Exit\n";
-    int choice;
-    cin >> choice;
 
-    if (choice == 5) break;
 
-    string name, category;
-    int price;
+      cout << "Choose a tree structure:\n";
+      cout << "1. Binary Search Tree\n";
+      cout << "2. Min-Heap\n";
+      cout << "3. Max-Heap\n";
+      cout << "4. AVL Tree\n";
+      cout << "5. Exit\n";
+      int choice;
+      cin >> choice;
 
-    cout << "Enter item name: ";
-    cin >> name;
-    cout << "Enter category: ";
-    cin >> category;
-    cout << "Enter price: ";
-    cin >> price;
+      if (choice == 5) break;
 
-    Item newItem(name, category, price);
+      cout << "Choose type of insertion:\n";
+      cout << "1. Read from a file\n";
+      cout << "2. manually\n";
+      int c;
+      cin >> c;
+      string file_name;
+      if (c == 1) {
+          cout << "enter file name " << endl;
+          cin >> file_name;
+          switch (choice) {
+              case 1:
+                  readItems(file_name, "bst");
+                  break;
+              case 2:
+                  readItems(file_name, "Min-Heap");
+                  break;
+              case 3:
+                  readItems(file_name, "Max-Heap");
+                  break;
+              case 4:
+                  readItems(file_name, "AVL");
+                  break;
+              default:
+                  cout << "Invalid choice. Try again.\n";
+                  break;
+          }
+      } else {
+          string name, category;
+          int price;
 
-    switch (choice) {
-      case 1:
-        bst.insert(newItem);
-        bst.inOrder();
-        break;
-      case 2:
-        minHeap.insert(newItem);
-        minHeap.print();
-        break;
-      case 3:
-        maxHeap.insert(newItem);
-        maxHeap.print();
-        break;
-      case 4:
-        avl.insert(newItem);
-        avl.inOrder();
-        break;
-      default:
-        cout << "Invalid choice. Try again.\n";
-        break;
-    }
+          cout << "Enter item name: ";
+          cin >> name;
+          cout << "Enter category: ";
+          cin >> category;
+          cout << "Enter price: ";
+          cin >> price;
+
+          Item newItem(name, category, price);
+
+          switch (choice) {
+              case 1:
+                  bst.insert(newItem);
+                  bst.inOrder();
+                  break;
+              case 2:
+                  minHeap.insert(newItem);
+                  minHeap.print();
+                  break;
+              case 3:
+                  maxHeap.insert(newItem);
+                  maxHeap.print();
+                  break;
+              case 4:
+                  avl.insert(newItem);
+                  avl.inOrder();
+                  break;
+              default:
+                  cout << "Invalid choice. Try again.\n";
+                  break;
+          }
+      }
   }
 }
